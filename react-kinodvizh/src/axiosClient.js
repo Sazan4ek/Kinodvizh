2fdd16dc29fd3a8e90343742d8decc35bb115fa1
Artyom <artterm24@gmail.com>
2025-05-16 22:17:18 +0300

    docker init

--- react-kinodvizh/src/axiosClient.js
+++ react-kinodvizh/src/axiosClient.js
@@ -3,7 +3,6 @@ import axios from "axios";
 const axiosClient = axios.create({
     baseURL: 'http://localhost:8000/api',
     withCredentials: true,
-    // validateStatus: (status) => status >= 200 && status < 300,
 });
 
 export default axiosClient;
